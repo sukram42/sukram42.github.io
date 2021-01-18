22893e69cf886ee40c3348a495e8e08579b70a3b
sukram42 <markus.boebel@tum.de>
2021-01-18 20:08:58 +0000

    Deploying to gh-pages from  @ 8ca6ee7852477842197bfd027f177b1ccb24f0ef 🚀

---
+++ precache-manifest.ea3346dffa8939e4592570c9267d8037.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ed04bdc86e93f7d997",
+    "url": "/css/chunk-vendors.6bc6ee4d.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d2c5955bd2a4fd67c542cfd937b7ed30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a367a4a7f04cc15ad5",
+    "url": "/js/about.86481416.js"
+  },
+  {
+    "revision": "53786c5c62e03dac5197",
+    "url": "/js/app.4e6073a9.js"
+  },
+  {
+    "revision": "33ed04bdc86e93f7d997",
+    "url": "/js/chunk-vendors.51c7bd67.js"
+  },
+  {
+    "revision": "0baf827439e037400b5eaa59e27d5838",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
