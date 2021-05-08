74db3d0983368f31706c28c9cdba0783d9f5210e
Markus Böbel <markus.boebel@tum.de>
2021-05-08 19:47:27 +0200

    Rebrand the page

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,107 @@
 <template>
     <v-main>
-      <div class="main">
-        <div class="left_part">
-          <LeftBanner></LeftBanner>
-        </div>
-        <div class="right_part">
-
-        </div>
+      <div class="slide1">
+          <div class="picture-and-intro" ref="logoText">
+              <img ref="logoImg" src="./assets/MBIcon.svg" class="logo">
+              <div class="header">
+                  <div class="name">Markus Böbel</div>
+                  <div class="study">M.Sc. Information System</div>
+              </div>
+              <div ref="inWork" class="inwork">
+                  This page is still in work
+              </div>
+          </div>
       </div >
+    <div class="slide2">
+
+    </div >
     </v-main>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import LeftBanner from "@/components/leftbanner.component.vue";
-
 export default Vue.extend({
   name: "App",
-
   components: {
-    LeftBanner
   },
+  methods: {
+    handleScroll: function(event: Event) {
+        // console.log(window.scrollY - this.oldScroll);
 
+        // this.$refs.logoText.style.top += (0.01 * (window.scrollY - this.oldScroll)) + "px";
+        this.oldScroll = window.scrollY;
+
+        const percentageOfMovement: number = window.scrollY / window.innerHeight;
+        console.log(percentageOfMovement);
+        if (this.$refs.logoImg) {
+            (this.$refs.logoImg as HTMLElement).style.width = (((1-percentageOfMovement)*25)+25)+"%";
+            (this.$refs.logoImg as HTMLElement).style.top = (((1-percentageOfMovement)*100)+100)+"px";
+        }
+        if (this.$refs.inWork) {
+            (this.$refs.inWork as HTMLElement).style.fontSize = (percentageOfMovement+1)+"em";
+        }
+    }
+  },
+  created() {
+      window.addEventListener('scroll', this.handleScroll);
+
+  },
   data: () => ({
-    //
+    oldScroll: 0,
+
   })
 });
 </script>
 <style lang="scss">
-  @import "variables";
+      @import "variables";
 
-  html, body {
-    font-family: "Segoe UI Light", sans-serif;
-  }
+      html, body {
+          font-family: "Roboto Mono", monospace;
+      }
+      .slide1 {
+          width: 100vw;
+          height: 100vh;
 
-  .main {
-    display: grid;
-    height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+      }
+      .name {
+          font-family: Roboto Mono, monospace;
+          font-style: normal;
+          font-weight: 300;
+          font-size: 2em;
+          line-height: 76px;
+      }
+      .study{
+          font-family: Roboto Mono, monospace;
+          font-style: normal;
+          font-weight: 100;
+          font-size: 1.2em;
 
-    @media only screen and (max-width: $breakpoint) {
-      grid-template-columns: 100%;
-      grid-template-rows: 100%;
-      grid-template-areas: "left";
-    }
-    @media only screen and (min-width: $breakpoint) {
-      grid-template-columns: 40% 60%;
-      grid-template-rows: auto;
-      grid-template-areas: "left right";
-    }
-  }
-  .left_part {
-    grid-area: left;
-    background-color: #444;
-  }
+      }
+      .logo{
+          width: 50%;
+      }
+      .picture-and-intro {
+          position: fixed;
+          display: flex;
+          align-items: center;
 
-  .right_part{
-    grid-area: right;
-  }
+          flex-direction: column;
+      }
+      .inwork{
 
-  .boldFont {
-    font-family: "Segoe UI";
-  }
+      }
+      .picture-and-intro > * {
+          padding: 1em;
+      }
+      .picture-and-intro * {
+          text-align: center;
+      }
+      .slide2 {
+          background-color: khaki;
+          height: 100vh;
+      }
+
 </style>
--- src/components/skill-chart.component.vue
+++ src/components/skill-chart.component.vue
@@ -7,8 +7,39 @@
         data: () =>({
             options: {
                 responsive: true,
-                maintainAspectRatio: false
-            }
+                maintainAspectRatio: false,
+                legend: {
+
+                    labels: {
+                        fontColor: "#FFF",
+                        fontSize: 18
+                    }
+                },
+                scaleShowHorizontalLines: false,
+                scales: {
+                    yAxes: [{
+                        gridLines:{
+                            display: false
+                        },
+                        ticks: {
+                            fontColor: "white",
+                            fontSize: 18,
+                            stepSize: 1,
+                            beginAtZero: true,
+                            barThickness: 1,
+                        }
+                    }],
+                    xAxes: [{
+                        barThickness:1,
+                        gridLines:{
+                            display: false
+                        },
+                        ticks: {
+                            display: false
+                        }
+                    }]
+                }
+                },
         }),
         mounted() {
             this.renderChart(this.dataset, this.options)
